edc7958d5c53e12fb1b13610bcaa507c3cdf116f
Anshul David <43073266+anshuldavid13@users.noreply.github.com>
2021-06-06 03:56:53 +0530

    Update countPrimes.java

--- java/leetcode/countPrimes.java
+++ java/leetcode/countPrimes.java
@@ -1,7 +1,6 @@
 /* https://leetcode.com/problems/count-primes/ */
 
 class Solution {
-    LinkedList<Integer> prime = new LinkedList<Integer>();
     
     public int countPrimes(int n) {
         
@@ -11,29 +10,42 @@ class Solution {
             return 0;
         if(n==2)
             return 0;
+                
+        var prime = new boolean[n];
+        int count = 0;
         
-        int count=1;
-        //HashMap<Integer,Integer> prime = new HashMap<Integer,Integer>();
-        
-        prime.add(2);
-        
-        for (int i=3;i<n;i++)
+        for (int i=2;i<n;i++)
         {
-            count= count+isPrime(i);
+           if(!prime[i])
+           {
+               count++;
+           // System.out.print(i+" ");
+           }
+            else
+                continue;
+            
+           for (int j=2;(i*j)<n;j++)
+           {
+               prime[i*j] = true;
+           }
         }
         return count;
         
     }
     
-    public int isPrime(int n)
+   /* public int isPrime(int n)
     {
        
         for(int i : prime)
         {
+            if(i > Math.sqrt(n))
+                break;
+            
             if(n%i==0)
                 return 0;
+            
         }
         prime.add(n);
         return 1;
-    }
+    } */
 }
